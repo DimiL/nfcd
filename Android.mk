8e3d93eb35e437f947d20f6ebf8ebfc18652e53a
allstars.chh <allstars.chh@gmail.com>
2013-08-21 15:20:19 +0800

    Fix issue #3, update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,6 @@
-# Copyright 2006 The Android Open Source Project
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #ifneq ($(TARGET_PROVIDES_NFCD),true) #{
 
@@ -31,9 +33,11 @@ LOCAL_SRC_FILES := \
     libnci/NfcTag.cpp \
     libnci/PeerToPeer.cpp \
     libnci/Pn544Interop.cpp \
-    libnci/IntervalTimer.cpp \
+    libnci/IntervalTimer.cpp
 
 LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/libnci \
+    $(LOCAL_PATH)/src \
     $(NFA)/include \
     $(NFA)/brcm \
     $(NFC)/include \
@@ -48,10 +52,7 @@ LOCAL_C_INCLUDES += \
     external/openssl/include \
     external/jansson/android \
     external/jansson/src \
-    bionic \
-    bionic/linker \
-    system/nfcd/libnci \
-    system/nfcd/src \
+    bionic
 
 LOCAL_SHARED_LIBRARIES += \
     libicuuc \
@@ -61,13 +62,12 @@ LOCAL_SHARED_LIBRARIES += \
     liblog \
     libnfc-nci \
     libstlport \
-    libjansson \
+    libjansson
 
 LOCAL_MODULE := nfcd
 LOCAL_MODULE_TAGS := debug
 
 LOCAL_CFLAGS := -DDEBUG -DPLATFORM_ANDROID -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1 -DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1 -DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1 -DHAVE_DLFCN_H=1 -DSILENT=1 -DNO_SIGNALS=1 -DNO_EXECUTE_PERMISSION=1 -D_GNU_SOURCE -D_REENTRANT -DUSE_MMAP -DUSE_MUNMAP -D_FILE_OFFSET_BITS=64 -DNO_UNALIGNED_ACCESS
-
 
 include $(BUILD_EXECUTABLE)
 
