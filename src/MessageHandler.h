24b5e521785ae9fa87430cef178b9fc14efae452
allstars.chh <allstars.chh@gmail.com>
2013-08-28 14:16:31 -0700

    remove dummy code

--- src/MessageHandler.h
+++ src/MessageHandler.h
@@ -9,24 +9,6 @@
 #include "NfcGonkMessage.h"
 #include <binder/Parcel.h>
 
-//enum {
-//  NOTIFY_NDEF_DISCOVERED = 1,
-//  NOTIFY_TECH_DISCOVERED,
-//  NOTIFY_READ_REQ,
-//  NOTIFY_WRITE_REQ,
-//  NOTIFY_TRANSCEIVE_REQ,
-//  NOTIFY_TRANSCEIVE_RSP,
-//  NOTIFY_NDEF_DETAILS_REQUEST,
-//  NOTIFY_READ_NDEF,
-//  NOTIFY_NDEF_WRITE_REQUEST,
-//  NOTIFY_NDEF_DISCONNECTED,
-//  NOTIFY_REQUEST_STATUS,
-//  NOTIFY_NDEF_PUSH_REQUEST,
-//  NOTIFY_SECURE_ELEMENT_ACTIVATE,
-//  NOTIFY_SECURE_ELEMENT_DEACTIVATE,
-//  NOTIFY_SECURE_ELEMENT_TRANSACTION
-//};
-
 class NativeNfcTag;
 class NdefMessage;
 
@@ -41,16 +23,7 @@ public:
 private:
   MessageHandler();
 
-//  static bool retrieveMessageType(const char *input, size_t length, unsigned int *outTypeId);
-
-//  static void messageNotifyNdefDiscovered(NdefMessage* ndefMsg);
-//  static void messageNotifyNdefDetails(int maxNdefMsgLength, int state);
-//  static void messageNotifyNdefDisconnected();
-//  static void messageNotifyNdefDisconnected(const char *message);
   static void notifyTechDiscovered(android::Parcel& parcel, void* data);
-//  static void messageNotifyRequestStatus(const char *requestId, int status, char *message);
-//  static void messageNotifySecureElementFieldActivated();
-//  static void messageNotifySecureElementFieldDeactivated();
 
 //  static bool handleWriteNdef(const char *input, size_t length);
 //  static bool handleNdefPush(const char *input, size_t length);
